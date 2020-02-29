5da64a5a481005a0c3f4ea7553c3670e04750510
siddamaso <61614941+siddamaso@users.noreply.github.com>
2020-02-29 23:43:52 +0800

    added back button in about application

--- app/src/main/java/com/example/mapping_project/AboutApp.java
+++ app/src/main/java/com/example/mapping_project/AboutApp.java
@@ -2,13 +2,28 @@ package com.example.mapping_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutApp extends AppCompatActivity {
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutApp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
