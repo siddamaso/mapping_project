f072326c84045c6d59c3b243b576949ff24b8172
siddamaso <61614941+siddamaso@users.noreply.github.com>
2020-02-29 15:45:19 +0800

    uploaded all the progress

---
+++ app/src/main/java/com/example/mapping_project/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.mapping_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    View HeaderView;
+    TextView nav_map, manual, about_app, about_us;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navi_drawer);
+
+        about_app = findViewById(R.id.nav_app);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        HeaderView = navigationView.getHeaderView(0);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_app) {
+
+            viewaboutapp();
+        }
+        else if (id == R.id.nav_about){
+
+            viewaboutus();
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void viewaboutus() {
+        Intent feedIntent = new Intent(MainActivity.this, AboutUs.class);
+        startActivity(feedIntent);
+    }
+
+    private void viewaboutapp() {
+        Intent feedIntent = new Intent(MainActivity.this, AboutApp.class);
+        startActivity(feedIntent);
+    }
+}
